ba762d65b85c3e233cd582bdfa739a4a941e99b9
javier martinez <jmartinez@fusionglobal.tech>
2025-08-21 11:27:49 -0300

    demo four

--- loop_curl.sh
+++ loop_curl.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 while true; do
-    # Mostrar solo la línea que contiene "Servidor escuchando"
-    oc logs deployment/hola-pod | grep "Servidor escuchando"
-    
-    # Esperar 3 segundos
+    # Obtener el nombre del primer pod que matchee la etiqueta "app=hola-pod"
+    POD=$(oc get pods -l app=hola-pod -o jsonpath='{.items[0].metadata.name}' 2>/dev/null)
+
+    if [ -n "$POD" ]; then
+        # Mostrar solo la línea que contiene tu mensaje
+        oc logs "$POD" --tail=1 | grep "Servidor escuchando"
+    else
+        echo "[$(date '+%Y-%m-%d %H:%M:%S')] No hay pods disponibles para 'hola-pod'"
+    fi
+
     sleep 3
 done
