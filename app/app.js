6209fa65eefa28bc2c506e66eea0d4ca6016ad79
javier martinez <jmartinez@fusionglobal.tech>
2025-09-29 16:57:29 -0300

    add change app

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const http = require("http");
 const podName = process.env.HOSTNAME || "desconocido";
 
 const server = http.createServer((req, res) => {
-  const message = `Hola! Desde el pod ${podName}`;
+  const message = `Hola!! Desde el pod ${podName}`;
   //console.log(message);   // 👈 se ve en logs
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end(message + "\n");
@@ -12,6 +12,6 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
-  console.log(`Servidor escuchando en puerto ${PORT}, Aloha! desde pod: ${podName}`);
+  console.log(`Servidor escuchando en puerto ${PORT}, Hola! desde pod: ${podName}`);
 });
 
